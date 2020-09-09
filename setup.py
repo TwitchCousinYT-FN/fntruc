eeb7c998e62117f60d8b2964dc15edfe98646dba
TwitchCousinYT-FN <68472807+TwitchCousinYT-FN@users.noreply.github.com>
2020-09-09 23:52:06 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='FNTwitchSetup',
+    version='0.0.1',
+    description='Private module',
+    url='',
+    author_email='admin@Bryna.com',
+    packages=find_packages(),
+    install_requires=['fortnitepy==1.7.1','sanic','requests','aiofiles']
+)
